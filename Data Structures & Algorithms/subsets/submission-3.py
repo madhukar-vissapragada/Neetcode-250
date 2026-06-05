81d5c462cf5607f04289ba720193b9c399d0d241
madhukar-vissapragada <95085208+madhukar-vissapragada@users.noreply.github.com>
2026-06-05 06:07:35 +0530

    Add: subsets - submission-3

---
+++ Data Structures & Algorithms/subsets/submission-3.py
@@ -0,0 +1,15 @@
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        result = []
+        self.solve(nums, 0, [], result)
+        return result 
+    
+    def solve(self, nums: List[int], current_index: int, current_subset: List[int], result: List[List[int]]):
+        if current_index == len(nums):
+            result.append(current_subset.copy())
+            return 
+        
+        current_subset.append(nums[current_index])
+        self.solve(nums, current_index + 1, current_subset, result)
+        current_subset.pop()
+        self.solve(nums, current_index + 1, current_subset, result)
