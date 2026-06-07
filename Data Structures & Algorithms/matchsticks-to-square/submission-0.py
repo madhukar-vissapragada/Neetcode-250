7e442bc153e241347f029e7b67c1fcc118b68a2e
madhukar-vissapragada <95085208+madhukar-vissapragada@users.noreply.github.com>
2026-06-07 12:26:03 +0530

    Add: matchsticks-to-square - submission-0

---
+++ Data Structures & Algorithms/matchsticks-to-square/submission-0.py
@@ -0,0 +1,29 @@
+class Solution:
+    def makesquare(self, nums: List[int]) -> bool:
+        total_sum = sum(nums)
+        k = 4
+        if total_sum % k != 0:
+            return False
+        req_sum = total_sum // k
+        already_opted = [0] * len(nums)
+        return self.solve(nums, k, 0, req_sum, req_sum, already_opted)
+    
+
+    def solve(self, nums, k, current_index, target_sum, req_sum, already_opted):
+        if k == 0:
+            return True
+        if target_sum == 0:
+            return self.solve(nums, k-1, 0, req_sum, req_sum, already_opted)
+        if current_index == len(nums):
+            return False
+
+        for i in range(current_index, len(nums)):       # ✅ try all remaining
+            if already_opted[i] == 0 and target_sum >= nums[i]:
+                already_opted[i] = 1
+                if self.solve(nums, k, i+1, target_sum - nums[i], req_sum, already_opted):
+                    return True
+                already_opted[i] = 0
+
+        return False
+  
+        
