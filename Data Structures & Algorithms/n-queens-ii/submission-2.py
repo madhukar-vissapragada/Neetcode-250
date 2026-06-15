543f9c30290d93b4c3fbdf998ce98285417c1681
madhukar-vissapragada <95085208+madhukar-vissapragada@users.noreply.github.com>
2026-06-15 06:37:12 +0530

    Add: n-queens-ii - submission-2

---
+++ Data Structures & Algorithms/n-queens-ii/submission-2.py
@@ -0,0 +1,62 @@
+class Solution:
+    def totalNQueens(self, n: int) -> int:
+        self.output = 0
+        result = []
+        configuration = [['.'] * n for _ in range(n)]
+        self.solve(0, configuration, result, n)
+        return self.output
+    
+    def solve(self, current_row: int, current_configuration: int, result: List[List[str]],  n: int):
+        if current_row == n:
+            self.output += 1
+            return 
+        
+        for current_col in range(n):
+            if self.is_valid(current_row, current_col, current_configuration, n):
+                current_configuration[current_row][current_col] = 'Q'
+                self.solve(current_row + 1, current_configuration, result, n)
+                current_configuration[current_row][current_col] = '.'
+         
+    
+
+    def is_valid(self, current_row: int, current_col: int, configuration: List[List[str]], n: int):
+        def is_row_valid(current_col: int, configuration: List[List[str]], n: int):
+            for row in range(n):
+                if configuration[row][current_col] == 'Q':
+                    return False 
+            
+            return True 
+        
+        def is_diagonally_valid(current_row: int, current_col: int, configuration: List[List[str]], n: int):
+            row = current_row
+            col = current_col 
+
+            while row >= 0 and col >= 0:
+                if configuration[row][col] == 'Q':
+                    return False 
+                
+                row -= 1 
+                col -= 1 
+            
+            row = current_row 
+            col = current_col 
+
+            while row >= 0 and col < n:
+                if configuration[row][col] == 'Q':
+                    return False 
+                
+                row -= 1 
+                col += 1
+            
+            return True 
+        
+
+        return (is_row_valid(current_col, configuration, n) and 
+                is_diagonally_valid(current_row, current_col, configuration, n))
+         
+
+
+
+
+
+
